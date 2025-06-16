86f7d1e395558ea2788e6ea7e1d7e270fee05b01
J-Lmb <joslmn@hotmail.com>
2025-06-16 19:13:37 +0200

    updated to avoid recreation of existing resources

--- infra_frontend/main.tf
+++ infra_frontend/main.tf
@@ -1,20 +1,19 @@
-resource "azurerm_resource_group" "frontend" {
-  name     = var.resource_group
-  location = var.location
+data "azurerm_resource_group" "frontend" {
+  name = var.resource_group
 }
 
 resource "azurerm_service_plan" "frontend" {
   name                = var.app_service_plan
-  location            = azurerm_resource_group.frontend.location
-  resource_group_name = azurerm_resource_group.frontend.name
+  location            = data.azurerm_resource_group.frontend.location
+  resource_group_name = data.azurerm_resource_group.frontend.name
   os_type             = "Linux"
   sku_name            = "B1"
 }
 
 resource "azurerm_linux_web_app" "frontend" {
   name                = var.app_name
-  location            = azurerm_resource_group.frontend.location
-  resource_group_name = azurerm_resource_group.frontend.name
+  location            = data.azurerm_resource_group.frontend.location
+  resource_group_name = data.azurerm_resource_group.frontend.name
   service_plan_id     = azurerm_service_plan.frontend.id
 
   site_config {
